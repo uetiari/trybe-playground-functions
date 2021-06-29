9e3d401cc150d15c34aff8218f4f075183d56514
Ariane Ueti <arianeueti@gmail.com>
2021-06-29 16:00:14 -0300

    Requisito #1 - compareTrue

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,10 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(value1, value2) {
+  if (value1 === true && value2 === true){
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // Desafio 2
